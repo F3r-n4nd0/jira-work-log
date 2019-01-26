c60b58361c061c4b24d411cc80458bca715c186c
Fernando Luna <337n4nd0@gmail.com>
2019-01-26 18:57:41 -0400

    Initial Commit

---
+++ jira-work-log/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jira-work-log
+//
+//  Created by Fernando Luna on 1/26/19.
+//  Copyright © 2019 Fernando Luna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
